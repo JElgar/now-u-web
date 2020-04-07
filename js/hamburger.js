3e3de97da599695f0e4188ffdfacb054444f0095
Jelgar <jamesnelgar@gmail.com>
2020-04-07 09:41:12 +0100

    Added hamburger menu on small devices

---
+++ js/hamburger.js
@@ -0,0 +1,9 @@
+function toggleHamburger() {
+  var x = document.getElementById("nav-bar-ul")
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block"
+  }
+}
+
