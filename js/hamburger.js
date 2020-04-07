dcaef78ba0273a15d088e99121c1921e23531827
Jelgar <jamesnelgar@gmail.com>
2020-04-07 11:38:11 +0100

    working on header media query

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,24 @@
 function toggleHamburger() {
   var x = document.getElementById("nav-bar-ul")
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var y = document.getElementById("nav-bar")
+  if (x.className === "show") {
+    //x.style.display = "none";
+    x.className - "hide"
+    y.style.height = "10%"
   } else {
-    x.style.display = "block"
+    //x.style.display = "block"
+    x.className = "show"
+    y.style.height = "100%"
+  }
+}
+
+function closeHamburger() {
+  if(window.matchMedia("(max-width: 820px)")) {
+    var x = document.getElementById("nav-bar-ul")
+    var y = document.getElementById("nav-bar")
+    //x.style.display = "none";
+    x.className = "hide"
+    y.style.height = "10%"
   }
 }
 
