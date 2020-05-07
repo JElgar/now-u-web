81651c0a3ff8b953eab2b47948fda3359afe2a3d
Le-Roi777 <56315245+Le-Roi777@users.noreply.github.com>
2020-05-07 09:27:05 +0200

    redesign_07_05_2020

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Redesign
 
+# V1-07-05-2020
+
 Updating a few aspects of the site to make it betterer
---
+++ js/validate.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var jQueryScript = document.createElement('script');
+
+    jQueryScript.setAttribute('src','https://s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js');
+    document.head.appendChild(jQueryScript);
+
+    (function($) {
+    
+        window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[1]='NAME';
+        ftypes[1]='text';
+        fnames[0]='EMAIL';
+        ftypes[0]='email';
+    }
+    (jQuery));var $mcj = jQuery.noConflict(true);
+    
+});
